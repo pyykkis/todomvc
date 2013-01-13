7c30acad91a311b80bb835ba12eb2ebacc1d3a36
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2013-01-13 09:57:13 +0200

    Hide #main and #footer when there are no todos.

--- labs/dependency-examples/bacon_transparency_require/js/app.js
+++ labs/dependency-examples/bacon_transparency_require/js/app.js
@@ -1,12 +1,21 @@
 (function() {
 
-  define([], function() {
+  define(['bacon'], function(Bacon) {
     var TodoApp;
     return TodoApp = (function() {
 
       function TodoApp(_arg) {
+        var showTodos, todos;
         this.el = _arg.el;
-        console.log(this.el);
+        this.main = this.el.find('#main');
+        this.footer = this.el.find('#footer');
+        todos = new Bacon.Bus();
+        showTodos = todos.map(function(todos) {
+          return todos.length > 0;
+        });
+        showTodos.onValue(this.main, 'toggle');
+        showTodos.onValue(this.footer, 'toggle');
+        todos.push([]);
       }
 
       return TodoApp;
--- labs/dependency-examples/bacon_transparency_require/src/app.coffee
+++ labs/dependency-examples/bacon_transparency_require/src/app.coffee
@@ -1,5 +1,18 @@
-define [], () ->
+define ['bacon'], (Bacon) ->
 
   class TodoApp
     constructor: ({@el}) ->
-      console.log @el
+      # Elements
+      @main   = @el.find '#main'
+      @footer = @el.find '#footer'
+
+      # Events and Properties
+      todos     = new Bacon.Bus()
+      showTodos = todos.map (todos) -> todos.length > 0
+
+      # Side effects
+      showTodos.onValue @main,   'toggle'
+      showTodos.onValue @footer, 'toggle'
+
+      # Kickstart
+      todos.push []
