abdda2de2a4dd6c103400be6168ba2287038a832
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2013-01-13 14:09:00 +0200

    Cleanup

--- labs/dependency-examples/bacon_transparency_require/js/app.js
+++ labs/dependency-examples/bacon_transparency_require/js/app.js
@@ -8,14 +8,16 @@
       ENTER_KEY = 13;
 
       function TodoApp(_arg) {
-        var deletedTodo, newTodo, todoListNotEmpty, todos;
+        var deletedTodo, newTodo, todoBus, todoListNotEmpty, todos, toggledTodo;
         this.el = _arg.el;
-        todos = new Bacon.Bus().log();
+        todoBus = new Bacon.Bus();
+        todos = todoBus.toProperty().log();
         newTodo = this.el.find('#new-todo').asEventStream('keyup').filter(function(e) {
           return e.keyCode === ENTER_KEY;
         }).map(function(e) {
           return {
-            todo: e.target.value.trim()
+            todo: e.target.value.trim(),
+            completed: false
           };
         }).filter(function(_arg1) {
           var todo;
@@ -23,6 +25,7 @@
           return todo.length > 0;
         });
         deletedTodo = this.el.find('#todo-list .destroy').asEventStream('click').map('.target.transparency.model');
+        toggledTodo = this.el.find('#todo-list .toggle').asEventStream('click').map('.target.transparency.model');
         todoListNotEmpty = todos.map(function(todos) {
           return todos.length > 0;
         });
@@ -30,27 +33,30 @@
           return {
             t: t
           };
-        }).decorateWith('ts', todos.toProperty()).onValue(function(_arg1) {
+        }).decorateWith('ts', todos).onValue(function(_arg1) {
           var t, ts;
           ts = _arg1.ts, t = _arg1.t;
-          return todos.push(ts.concat([t]));
+          return todoBus.push(ts.concat([t]));
         });
-        deletedTodo.map(function(d) {
+        deletedTodo.map(function(t) {
           return {
             d: d
           };
-        }).decorateWith('ts', todos.toProperty()).onValue(function(_arg1) {
+        }).decorateWith('ts', todos).onValue(function(_arg1) {
           var d, ts;
           ts = _arg1.ts, d = _arg1.d;
-          return todos.push(ts.filter(function(t) {
+          return todoBus.push(ts.filter(function(t) {
             return t !== d;
           }));
         });
+        toggledTodo.onValue(function(t) {
+          return t.completed = !t.completed;
+        });
         newTodo.onValue(this.el.find('#new-todo'), 'val', '');
-        todos.onValue(this.el.find('#todo-list'), 'render');
         todoListNotEmpty.onValue(this.el.find('#main'), 'toggle');
         todoListNotEmpty.onValue(this.el.find('#footer'), 'toggle');
-        todos.push([]);
+        toggledTodo.map(todos).merge(todos).onValue(this.el.find('#todo-list'), 'render');
+        todoBus.push([]);
       }
 
       return TodoApp;
--- labs/dependency-examples/bacon_transparency_require/src/app.coffee
+++ labs/dependency-examples/bacon_transparency_require/src/app.coffee
@@ -4,35 +4,47 @@ define ['bacon'], (Bacon) ->
     ENTER_KEY = 13
 
     constructor: ({@el}) ->
-      todos = new Bacon.Bus().log()
+      todoBus = new Bacon.Bus()
+      todos   = todoBus.toProperty().log()
 
       newTodo = @el.find('#new-todo')
         .asEventStream('keyup')
         .filter((e) -> e.keyCode == ENTER_KEY)
-        .map((e) -> todo: e.target.value.trim())
+        .map((e) -> todo: e.target.value.trim(), completed: false)
         .filter(({todo}) -> todo.length > 0)
 
       deletedTodo = @el.find('#todo-list .destroy')
         .asEventStream('click')
         .map('.target.transparency.model')
 
+      toggledTodo = @el.find('#todo-list .toggle')
+        .asEventStream('click')
+        .map('.target.transparency.model')
+
       todoListNotEmpty = todos.map((todos) -> todos.length > 0)
+
+      # Side effects
 
       # Add new todo to the list
       newTodo.map((t) -> t: t)
-        .decorateWith('ts', todos.toProperty())
-        .onValue(({ts, t}) -> todos.push ts.concat [t])
+        .decorateWith('ts', todos)
+        .onValue(({ts, t}) -> todoBus.push ts.concat [t])
 
       # Delete todo from the list
-      deletedTodo.map((d) -> d: d)
-        .decorateWith('ts', todos.toProperty())
-        .onValue(({ts, d}) -> todos.push ts.filter (t) -> t != d)
+      deletedTodo.map((t) -> d: d)
+        .decorateWith('ts', todos)
+        .onValue(({ts, d}) -> todoBus.push ts.filter (t) -> t != d)
 
-      # Side effects
-      newTodo.onValue          @el.find('#new-todo'),  'val', ''
-      todos.onValue            @el.find('#todo-list'), 'render'
-      todoListNotEmpty.onValue @el.find('#main'),      'toggle'
-      todoListNotEmpty.onValue @el.find('#footer'),    'toggle'
+      # Toggle todo
+      toggledTodo.onValue((t) -> t.completed = !t.completed)
+
+      # Update view
+      newTodo.onValue          @el.find('#new-todo'), 'val', ''
+      todoListNotEmpty.onValue @el.find('#main'),     'toggle'
+      todoListNotEmpty.onValue @el.find('#footer'),   'toggle'
+
+      toggledTodo.map(todos)
+        .merge(todos).onValue @el.find('#todo-list'), 'render'
 
       # Kickstart
-      todos.push []
+      todoBus.push []
