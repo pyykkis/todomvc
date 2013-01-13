bad94f714686dddc507f71fa0cf59a5237fba9bc
Jarno Keskikangas <jarno.keskikangas@leonidasoy.fi>
2013-01-13 13:30:13 +0200

    Delete todos from the list.

--- labs/dependency-examples/bacon_transparency_require/js/app.js
+++ labs/dependency-examples/bacon_transparency_require/js/app.js
@@ -8,7 +8,7 @@
       ENTER_KEY = 13;
 
       function TodoApp(_arg) {
-        var newTodo, showTodos, todos;
+        var deletedTodo, newTodo, todoListNotEmpty, todos;
         this.el = _arg.el;
         todos = new Bacon.Bus().log();
         newTodo = this.el.find('#new-todo').asEventStream('keyup').filter(function(e) {
@@ -22,7 +22,8 @@
           todo = _arg1.todo;
           return todo.length > 0;
         });
-        showTodos = todos.map(function(todos) {
+        deletedTodo = this.el.find('#todo-list .destroy').asEventStream('click').map('.target.transparency.model');
+        todoListNotEmpty = todos.map(function(todos) {
           return todos.length > 0;
         });
         newTodo.map(function(t) {
@@ -34,10 +35,21 @@
           ts = _arg1.ts, t = _arg1.t;
           return todos.push(ts.concat([t]));
         });
-        todos.onValue(this.el.find('#todo-list'), 'render');
-        showTodos.onValue(this.el.find('#main'), 'toggle');
-        showTodos.onValue(this.el.find('#footer'), 'toggle');
+        deletedTodo.map(function(d) {
+          return {
+            d: d
+          };
+        }).decorateWith('ts', todos.toProperty()).onValue(function(_arg1) {
+          var d, ts;
+          ts = _arg1.ts, d = _arg1.d;
+          return todos.push(ts.filter(function(t) {
+            return t !== d;
+          }));
+        });
         newTodo.onValue(this.el.find('#new-todo'), 'val', '');
+        todos.onValue(this.el.find('#todo-list'), 'render');
+        todoListNotEmpty.onValue(this.el.find('#main'), 'toggle');
+        todoListNotEmpty.onValue(this.el.find('#footer'), 'toggle');
         todos.push([]);
       }
 
--- labs/dependency-examples/bacon_transparency_require/src/app.coffee
+++ labs/dependency-examples/bacon_transparency_require/src/app.coffee
@@ -4,25 +4,35 @@ define ['bacon'], (Bacon) ->
     ENTER_KEY = 13
 
     constructor: ({@el}) ->
-       # Events and Properties
       todos = new Bacon.Bus().log()
 
-      newTodo = @el.find('#new-todo').asEventStream('keyup')
+      newTodo = @el.find('#new-todo')
+        .asEventStream('keyup')
         .filter((e) -> e.keyCode == ENTER_KEY)
         .map((e) -> todo: e.target.value.trim())
         .filter(({todo}) -> todo.length > 0)
 
-      showTodos = todos.map((todos) -> todos.length > 0)
+      deletedTodo = @el.find('#todo-list .destroy')
+        .asEventStream('click')
+        .map('.target.transparency.model')
 
-      # Side effects
+      todoListNotEmpty = todos.map((todos) -> todos.length > 0)
+
+      # Add new todo to the list
       newTodo.map((t) -> t: t)
         .decorateWith('ts', todos.toProperty())
         .onValue(({ts, t}) -> todos.push ts.concat [t])
 
-      todos.onValue     @el.find('#todo-list'), 'render'
-      showTodos.onValue @el.find('#main'),      'toggle'
-      showTodos.onValue @el.find('#footer'),    'toggle'
-      newTodo.onValue   @el.find('#new-todo'),  'val', ''
+      # Delete todo from the list
+      deletedTodo.map((d) -> d: d)
+        .decorateWith('ts', todos.toProperty())
+        .onValue(({ts, d}) -> todos.push ts.filter (t) -> t != d)
+
+      # Side effects
+      newTodo.onValue          @el.find('#new-todo'),  'val', ''
+      todos.onValue            @el.find('#todo-list'), 'render'
+      todoListNotEmpty.onValue @el.find('#main'),      'toggle'
+      todoListNotEmpty.onValue @el.find('#footer'),    'toggle'
 
       # Kickstart
       todos.push []
